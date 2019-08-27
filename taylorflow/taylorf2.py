11a2a4958736b71c055bef3b893d4072cae0ebdd
MatthewSWoods <51963172+MatthewSWoods@users.noreply.github.com>
2019-08-27 20:24:38 +0100

    Update taylorf2.py

--- taylorflow/taylorf2.py
+++ taylorflow/taylorf2.py
@@ -1,25 +1,19 @@
-#Preamble
 import tensorflow as tf
 #enable Eager execution
 tf.enable_eager_execution()
 
 import numpy as np
-import matplotlib.pyplot as plt
 
-#Start Interactive Session
-sess = tf.InteractiveSession()
-
-def PN_phases(freq,mTot,eta,t_c,phi_c,phase_order):
+def PN_phases(freq,mTot,eta,phase_order):
     """
     PN phase orders for TaylorF2
     """
     
     #constants in equation
-    piM = tf.multiply(mTot*4.96e-6,np.pi) # Total mass times pi
+    piM = tf.multiply(mTot*4.92549102554e-6,np.pi) # Total mass times pi
     v = tf.pow(tf.multiply(piM,freq),(1/3)) # characteristic velocity of binary
     etasq = tf.pow(eta,2) # square of the symmetric mass ratio
     etacb= tf.pow(eta,3) # cube of the symmetric mass ratio
-    vISCO = 1. / tf.sqrt(6.) # velocity at innermost stable circular orbit
     gamma = 0.577215664901532 #Euler–Mascheroni constant
     
     #v parameters
@@ -28,34 +22,24 @@ def PN_phases(freq,mTot,eta,t_c,phi_c,phase_order):
     v3 = tf.pow(v,3)
     v4 = tf.pow(v,4)
     v5 = tf.pow(v,5)
-    v5log = tf.log(tf.divide(v,vISCO))
+    v5log = tf.log(v)
     v6 = tf.pow(v,6)
     v6log = tf.log(4*v)
     v7 = tf.pow(v,7)
     
     #produce PN coeffiecients
-    PN_cnst = tf.add((2*np.pi*t_c*freq),(-phi_c))
     P0 = tf.multiply((3./128),tf.reciprocal(eta))
-#     P2 = tf.multiply(tf.add(743/84,((11)*eta)),(5/9))
-#     P3 = (-16*np.pi)
-#     P4 = tf.multiply(tf.add((3058673/1016064),tf.add(((5429/1008)*eta),(617/144)*etasq)),(10))
-#     P5 = tf.multiply(tf.add(38645/756,(-(65/9)*eta)),(np.pi))
-#     Pl5 = tf.multiply(tf.multiply(tf.add(38645/756,(-(65/9)*eta)),(np.pi)),3)
-#     P6 = tf.add(((11583231236531/4694215680)-(640*np.pi*np.pi/3)-(6848*gamma/21)),\
-#          tf.add(((((-15737765635/3048192)+(2255*np.pi*np.pi/12)))*eta),\
-#          tf.add(((76055/1728)*etasq),(-127825/1296)*etacb)))
-#     Pl6 = -(6848/21)
-#     P7 = tf.multiply(tf.add((77096675/254016),tf.add(((378515/1512)*eta),(-74045/756)*etasq)),(np.pi))
+    P2 = tf.multiply(tf.add(743/84,((11)*eta)),(5/9))
+    P3 = (-16*np.pi)
+    P4 = tf.multiply(tf.add((3058673/1016064),tf.add(((5429/1008)*eta),(617/144)*etasq)),(10))
+    P5 = tf.multiply(tf.add(7729/84,(-13*eta)),(np.pi*5/9))
+    Pl5 = tf.multiply(tf.add(7729/84,(-13*eta)),(np.pi*5/3))
+    P6 = tf.add(((11583231236531/4694215680)-(640*np.pi*np.pi/3)-(6848*gamma/21)),\
+         tf.add(((((-15737765635/3048192)+(2255*np.pi*np.pi/12)))*eta),\
+         tf.add(((76055/1728)*etasq),(-127825/1296)*etacb)))
+    Pl6 = -(6848/21)
+    P7 = tf.multiply(tf.add((77096675/254016),tf.add(((378515/1512)*eta),(-74045/756)*etasq)),(np.pi))
 
-    # For setting terms to zero 
-    P2 = 0.
-    P3 = 0.
-    P4 = 0.
-    P5 = 0.
-    Pl5 = 0.
-    P6 = 0.
-    Pl6 = 0.
-    P7 = 0.
     
     #Produce full PN terms
     PN0 = tf.multiply(P0,v0)
@@ -67,8 +51,24 @@ def PN_phases(freq,mTot,eta,t_c,phi_c,phase_order):
     PN7 = tf.multiply(P7,v7)
     
     #phases = PN_cnst + PN0(1+PN1*v+PN2*v2+PN3*v3+PN4+PN5+PN6+PN7)
-    phases = tf.add(PN_cnst,tf.multiply(PN0,(1+PN2+PN3+PN4+PN5+PN6+PN7)))
- 
+    if phase_order == 7:
+        phases = tf.multiply(PN0,(1+PN2+PN3+PN4+PN5+PN6+PN7))
+    elif phase_order == 6:
+        phases = tf.multiply(PN0,(1+PN2+PN3+PN4+PN5+PN6))
+    elif phase_order == 5:
+        phases = tf.multiply(PN0,(1+PN2+PN3+PN4+PN5))
+    elif phase_order == 4:
+        phases = tf.multiply(PN0,(1+PN2+PN3+PN4)) 
+    elif phase_order == 3:
+        phases = tf.multiply(PN0,(1+PN2+PN3))
+    elif phase_order == 2:
+        phases = tf.multiply(PN0,(1+PN2))
+    elif phase_order == 1:
+        phases = PN0
+    else:
+        phases = PN0
+        
+
     return phases
                          
 def PN_amplitude(freq,ChirpMass,LuminosityDistance):
@@ -79,14 +79,14 @@ def PN_amplitude(freq,ChirpMass,LuminosityDistance):
     #returns the amplitude of the 0PN waveform
     #arguments are chirp mass, luminosity distance and frequencies
                          
-    term1 = tf.reciprocal(LuminosityDistance/3e8)# divide by factor of c to convert to units of time
-    term2 = tf.multiply(np.sqrt(5/24)*(np.pi**(-2/3)),tf.pow(ChirpMass*4.96e-6,(5/6))) #times by mtsol to get units of time
+    term1 = tf.reciprocal(LuminosityDistance/299792458)# divide by factor of c to convert to units of time
+    term2 = tf.multiply(np.sqrt(5/24)*(np.pi**(-2/3)),tf.pow(ChirpMass*4.92549102554e-6,(5/6))) #times by mtsol to get units of time
     term3 = tf.pow(freq,(-7/6))
                          
     return tf.multiply(term1,tf.multiply(term2,term3))
                          
-def getwaveform(mass1,mass2,frequencies=None,LuminosityDistance=1.,t_c=0,phi_c=0.,f_low=10.,
-               df=0.1,f_high=1600.,phase_order=0):
+def getwaveform(mass1,mass2,frequencies=None,LuminosityDistance=1.,f_low=10.,
+               df=1./512,f_high=1600.,phase_order=0):
     """
     TaylorFlow Main Function
     
@@ -99,27 +99,29 @@ def getwaveform(mass1,mass2,frequencies=None,LuminosityDistance=1.,t_c=0,phi_c=0
     
     #get total mass,chirp mass and symetric mass ratio for use later in function
     M = tf.add(mass1,mass2)
-    eta = tf.divide(tf.multiply(mass1,mass2),tf.square(M))
+    eta = tf.divide(tf.multiply(mass1,mass2),tf.pow(M,2))
     ChirpMass = tf.multiply(M,tf.pow(eta,(3/5))) #Chirp mass is given by M*(eta^3/5)   
     
     #define fISCO
-    f_ISO = 1/(6.**1.5*np.pi*(M)*4.93e-6) #frequency of innermost stable circular orbit (SI units)
+    f_ISO = 1/(6.**1.5*np.pi*(M)*4.92549102554e-6) #frequency of innermost stable circular orbit (SI units)
     
     #define frequencies
     if frequencies is None:
         f = np.arange(1.,f_high,df)
         frequencies = tf.Variable(f,name= "frequencies",dtype= tf.float32)
-    else:  
+    else: 
+        frequencies[0] = 1
         frequencies = tf.Variable(frequencies,name= "frequencies",dtype= tf.float32)
         
+
     #other constants                     
     L_D =  tf.constant((3.086e+22*LuminosityDistance),name="LuminosityDistance",dtype=tf.float32) 
     
     #get phases at 0PN order
-    phase = PN_phases(frequencies,M,eta,t_c,phi_c,phase_order)
-    amp = PN_amplitude(frequencies,ChirpMass,L_D)
+    phase = PN_phases(frequencies, M, eta, phase_order)
+    amp = PN_amplitude(frequencies, ChirpMass, L_D)
     
-    #set amplitude to zero where the waveform results are unphysical
+    #set amplitude to zero where the waveform results are unphysical/unwanted
     cond1 = frequencies<f_low #below f_low
     ind1 = tf.where(cond1)
     cond2 = frequencies>f_ISO #above fISCO
@@ -128,7 +130,7 @@ def getwaveform(mass1,mass2,frequencies=None,LuminosityDistance=1.,t_c=0,phi_c=0
     amp = tf.scatter_update(tf.Variable(amp), ind2, tf.constant(0.))
     
     #Calculte waveform
-    iphase = tf.multiply(tf.complex(0.,1.),tf.complex(phase,0.))
+    iphase = tf.multiply(tf.complex(0.,1.),tf.complex(phase-(np.pi/4)+np.pi,0.))
     waveform = tf.multiply(tf.complex(amp,0.),tf.exp(iphase))
-    
-    return frequencies.numpy(), waveform
+     
+    return frequencies, waveform
