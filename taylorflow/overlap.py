e7d882485f8a1ef693ba4cf15e6f398013e5db5e
MatthewSWoods <51963172+MatthewSWoods@users.noreply.github.com>
2019-08-27 20:26:02 +0100

    Update overlap.py

--- taylorflow/overlap.py
+++ taylorflow/overlap.py
@@ -1,54 +1,77 @@
-#Preamble
-import tensorflow as tf
-#enable Eager execution
-tf.enable_eager_execution()
+def tensornorm(a):
+    """divides by max value of tensor to normalise between 0 and 1"""
+    TensorMax = tf.reduce_max(tf.abs(a))
+    norm_condition = tf.complex(tf.reciprocal(TensorMax),0.)
+    
+    return tf.multiply(a,norm_condition),norm_condition
 
-def cmplxInnerProd(temp,data,psd,df):
-    "computes complex inner product in the fourier domain IP = 4 deltaf sum((a * conguagte(b))/Weights)"
+def ComplexInnerProduct(temp,data,psd,df):
+    """computes complex inner product in the fourier domain IP = 4 deltaf sum((a * conguagte(b))/Weights)"""
     
     weights = tf.sqrt(psd)
     norm = (4*df)
-    a_weight = tf.divide(temp,weights)
+    a_weight = tf.div_no_nan(temp,weights)
     b_conj = tf.conj(data)
-    b_weight = tf.divide(tf.cast(b_conj,dtype=tf.complex64),weights)
+    b_weight = tf.div_no_nan(tf.cast(b_conj,dtype=tf.complex64),weights)
     a_dot_b = tf.reduce_sum(tf.multiply(a_weight,b_weight))
     
     return tf.multiply(norm,a_dot_b)
 
-def InnerProd(temp,data,psd,df):
-    "computes inner product in the fourier domain IP = 4 deltaf RE sum((a * conguagte(b))/Weights)"
+def InnerProduct(temp,data,psd,df):
+    """computes inner product in the fourier domain IP = 4 deltaf RE sum((a * conguagte(b))/Weights)"""
     
-    return tf.real(cmplxInnerProd(temp,data,psd,df))
+    return tf.real(ComplexInnerProduct(temp,data,psd,df))
 
-def sigsq(temp, psd, df):
-    "computes sig^2 = (a|a), which can then be used to normalise function "
+def matchedfilter(temp,data,psd,df):
+    """Computes the overlap between two waveforms"""
     
-    weights = tf.sqrt(psd)
-    norm = (4*df)
-    a_weight = tf.divide(temp,weights)
-    b_conj = tf.conj(temp)
-    b_weight = tf.divide(tf.cast(b_conj,dtype=tf.complex64),weights)
-    a_dot_b = tf.reduce_sum(tf.multiply(a_weight,b_weight))
+    prod_a_b = InnerProduct(temp,data,psd,df)
+    prod_a_a = InnerProduct(temp,temp,psd,df)
+    prod_b_b = InnerProduct(data,data,psd,df)
     
-    return tf.real(tf.multiply(norm,a_dot_b))
+    sigma = tf.sqrt(tf.multiply(prod_a_a,prod_b_b))
+    
+    return tf.divide(prod_a_b,sigma)
 
-def sig(temp, psd, df):
-    "returns the sigma value of the signal"
+def match(temp,data,psd,df,freq,tc_low = -1.,tc_high = 1.,samples=201):
+    """Calculate the max match between two waveforms by maximisiung over coalescence time"""
     
-    return tf.sqrt(sigsq(temp, psd, df))
+    t_c = tf.cast(tf.linspace(tc_low,tc_high,samples),dtype=tf.float32)
+    freq_tc = tf.tensordot(t_c,freq,axes=0)
+    freq_tc = tf.cast(freq_tc,dtype=tf.complex64)
+    shift_factor = tf.exp(1j*2*np.pi*freq_tc)
 
-def tensornorm(a):
-    "divides by max value of tensor to normalise between 0 and 1"
-    TensorMax = tf.reduce_max(tf.abs(a))
-    Tmax_cmplx_recip = tf.complex(tf.reciprocal(TensorMax),0.)
+    match_max = np.zeros(len(t_c))
+    for coa_time in range(len(t_c)):
+        waveform_shift = shift_factor[coa_time,:]
+        waveform_shifted = tf.multiply(waveform_shift,temp)
+        match_max[coa_time] = matchedfilter(waveform_shifted,data,psd,df)
     
-    return tf.multiply(a,Tmax_cmplx_recip),TensorMax
+    return t_c, match_max
 
-def overlap(temp,data,psd,df):
-    "Overlap equation"
+def match_cpu(temp,data,psd,df,freq,norm_condition,tc_low = -1.,tc_high = 1.,samples=201):
+    """Calculate the max match between two waveforms by maximisiung over coalescence time, calculated on the cpu"""
     
-    norm = tf.divide(1,tf.multiply(sig(temp,psd,df),sig(data,psd,df)))
-    inner = InnerProd(temp,data,psd,df)
-    overlap = tf.multiply(norm,inner)
-        
-    return overlap
+    with tf.device('/cpu:0'):
+        t_c = tf.cast(tf.linspace(tc_low,tc_high,samples),dtype=tf.float32)
+        freq_tc = tf.tensordot(t_c,freq,axes=0)
+        freq_tc = tf.cast(freq_tc,dtype=tf.complex64)
+        shift_factor = tf.exp(1j*2*np.pi*freq_tc)
+
+        match_max = np.zeros(len(t_c))
+        for coa_time in range(len(t_c)):
+            waveform_shift = shift_factor[coa_time,:]
+            waveform_shifted = tf.multiply(waveform_shift,temp)
+            match_max[coa_time] = matchedfilter(waveform_shifted,data,psd,df)
+    
+    return t_c, match_max
+
+def loglikelihood(temp,data,psd,df):
+    """Computes the relative log likelihood of two data sets, computed by <data|temp>-1/2<temp|temp> """
+    return (InnerProduct(data,temp,psd,df)-0.5*InnerProduct(temp,temp,psd,df))
+
+
+def SNR(temp,data,psd,df):
+    """Compute the SNR at the true value of the parameters """
+
+    return tf.sqrt(2*loglikelihood(temp,data,psd,df))
